728b7c90628016126a88098ad5c87d164c2becb6
Cole Rau <colevrau@gmail.com>
2021-01-16 18:33:34 -0800

    updated linked list such that it is now nested

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-let linkedList = {
-  "head": {value: 1, next: "address_two"},
-  "address_two": {value: 2, next: "address_three"},
-  "address_three": {value: 3, next: "address_four"},
-  "address_four": {value: 4, next: "tail"},
-  "tail": {value: 5, next: null}
-}
+// let linkedList = {
+//   "head": {value: 1, next: "address_two"},
+//   "address_two": {value: 2, next: "address_three"},
+//   "address_three": {value: 3, next: "address_four"},
+//   "address_four": {value: 4, next: "tail"},
+//   "tail": {value: 5, next: null}
+// }
+
+const linkedList = {
+  head: {
+      value: 6,
+      next: {
+          value: 10,                                          
+          next: {
+              value: 12,
+              next: {
+                  value: 3,
+                  next: null	
+                  }
+              }
+          }
+      }
+  }
 
 reverseList = list => {
   
   let prev = null
   let next = null
   let head = list.head
-  console.log(head.next)
+  // console.log(head.next)
 
-  // while (head !== null) {
-  //   next = head.next
-  //   head.next = prev
-  //   prev = head
-  //   head = next
+  while (head !== null) {
+    next = head.next
+    head.next = prev
+    prev = head
+    head = next
 
 
 
-  // }
+  }
   return prev
 }
 
 let rev = reverseList(linkedList)
 
-// console.log(rev)
+console.log(rev)
