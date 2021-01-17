a21a80b6ab47a208690bbbe31135aa0b8e0c0b03
Cole Rau <colevrau@gmail.com>
2021-01-16 17:15:42 -0800

    wrote linked list example and set up function skeleton

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("greetings")
+let linkedList = {
+  "head": {value: 1, next: "address_two"},
+  "address_two": {value: 2, next: "address_three"},
+  "address_three": {value: 3, next: "address_four"},
+  "address_four": {value: 4, next: "tail"},
+  "tail": {value: 5, next: null}
+}
+
+reverseList = list => {
+  console.log(list.head.value)
+}
+
+// console.log(linkedList)
+
+let rev = reverseList(linkedList)
+
+// console.log(rev)
