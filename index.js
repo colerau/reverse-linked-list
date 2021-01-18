eb6e00aa1d7fc23ae982f1275d1ffe269d5040a6
Cole Rau <colevrau@gmail.com>
2021-01-17 18:30:41 -0800

    added linkedlist and node classes

--- index.js
+++ index.js
@@ -6,21 +6,21 @@
 //   "tail": {value: 5, next: null}
 // }
 
-const linkedList = {
-  head: {
-      value: 6,
-      next: {
-          value: 10,                                          
-          next: {
-              value: 12,
-              next: {
-                  value: 3,
-                  next: null	
-                  }
-              }
-          }
-      }
-  }
+// const linkedList = {
+//   head: {
+//       value: 6,
+//       next: {
+//           value: 10,                                          
+//           next: {
+//               value: 12,
+//               next: {
+//                   value: 3,
+//                   next: null	
+//                   }
+//               }
+//           }
+//       }
+//   }
 
 reverseList = list => {
   
@@ -41,6 +41,30 @@ reverseList = list => {
   return prev
 }
 
-let rev = reverseList(linkedList)
+// let rev = reverseList(linkedList)
 
-console.log(rev)
+// console.log(rev)
+
+class ListNode {
+  constructor(data) {
+    this.data = data
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head
+  }
+}
+
+let node1 = new ListNode(5)
+let node2 = new ListNode(3)
+let node3 = new ListNode(9)
+
+node1.next = node2
+node2.next = node3
+
+let list = new LinkedList(node1)
+
+console.log(list)
